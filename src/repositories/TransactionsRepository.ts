cb36a873929bbf38ea26dc8454a20a23268786a2
caionamoratto <caionamoratto@hotmail.com>
2020-09-04 14:25:17 -0300

    Desafio Fundamentos Node Feito

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,8 +6,16 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO{
+  title: string, 
+  value: number,
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
+
   private transactions: Transaction[];
+
 
   constructor() {
     this.transactions = [];
@@ -15,14 +23,49 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    
+    return this.transactions;
+
   }
 
   public getBalance(): Balance {
     // TODO
+    
+    var income : number = 0;
+    var outcome : number = 0;
+    var total  = 0;
+
+    for(var i = 0; i < this.transactions.length; i++){
+      if(this.transactions[i].type == 'income'){
+        income += this.transactions[i].value;
+        total += this.transactions[i].value;
+      }else if(this.transactions[i].type == 'outcome'){
+        outcome += this.transactions[i].value;
+        total -= this.transactions[i].value;
+      }
+    }
+
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({title, value, type});
+
+    if(transaction.type == 'income'){
+      this.transactions.push(transaction);
+    }else if(transaction.type == 'outcome') {
+      if((this.getBalance().total - transaction.value) < 0){
+
+        throw Error('Não há saldo disponível para transação');
+        console.log(this.transactions);
+
+      }else{
+        this.transactions.push(transaction);
+      }
+    }
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,15 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+interface RequestDTO{
+
+  title: string,
+  value: number,
+  type: 'income' | 'outcome';
+
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +17,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({title, value, type}: RequestDTO): Transaction {
     // TODO
+
+    const transaction = this.transactionsRepository.create(
+      {
+          title, 
+          value,
+          type,
+    });
+
+    if(type != 'income' && type != 'outcome'){
+      throw Error('Erro de tipo ao criar transação');
+    }
+
+    return transaction;
   }
 }
 
